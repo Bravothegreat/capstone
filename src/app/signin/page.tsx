50c324e5ebdce89f7e37fc9fdb7e140b4cf3da21
bravothegreat <okwudilijohngreat@gmail.com>
2024-07-27 23:57:51 +0100

    edited the sigin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,42 +1,209 @@
+// "use client";
+// import React, { useState } from "react";
+// import  { useRouter } from "next/navigation";
+// import { signInWithEmailAndPassword,  signInWithPopup } from "firebase/auth";
+// import { auth, firestore } from "../firebase/firebase";
+// import { doc, getDoc, setDoc } from "firebase/firestore";
+// import Link from "next/link";
+// import { googleAuthProvider } from "../firebase/firebase";
+
+//  const Signin = () => {
+//   const [email, setEmail] = useState("");
+  
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState<string | null>(null);
+//   const router = useRouter();
+
+//   const handleSiginwithGoogle = async () => {
+//     try {
+//       const userCredential = await signInWithPopup(auth, googleAuthProvider);
+//       console.log(userCredential);
+
+//       const user = userCredential.user;
+//       if (user.emailVerified) {
+//         // retrive user data from local storage
+
+//         const registrationData = localStorage.getItem("registrationData");
+//         const {
+//           firstName = "",
+//           lastName = "",
+//           password = "",
+//         } = registrationData ? JSON.parse(registrationData) : {};
+
+//         //check if user data is valid and exist in firebase
+
+//         const userDoc = await getDoc(doc(firestore, "user", user.uid));
+//         if (!userDoc.exists()) {
+
+//           // save user data to firestre after email verification
+
+//           await setDoc(doc(firestore, "user", user.uid), {
+//             firstName,
+//             lastName,
+//             password,
+//             email: user.email,
+//           });
+//         }
+//         router.push("/dashboard");
+//       } else {
+//         setError("please verify your email before logging in");
+//       }
+//     } catch (error) {
+//       if (error instanceof Error) {
+//         setError(error.message);
+//       } else {
+//         setError("An unknown error occurred");
+//       }
+//     }
+//   };
+
+//   const handleSigin = async (event: React.FormEvent) => {
+//     event.preventDefault();
+//     setError(null);
+
+//     try {
+//       const userCredential = await signInWithEmailAndPassword(
+//         auth,
+//         email,
+//         password
+//       );
+
+//       const user = userCredential.user;
+//       if (user.emailVerified) {
+//         // retrive user data from local storage
+
+//         const registrationData = localStorage.getItem("registrationData");
+//         const {
+//           firstName = "",
+//           lastName = "",
+//           password = "",
+//         } = registrationData ? JSON.parse(registrationData) : {};
+
+//         // check if user data is valid and exist in firebase
+
+//         const userDoc = await getDoc(doc(firestore, "user", user.uid));
+//         if (!userDoc.exists()) {
+//           await setDoc(doc(firestore, "user", user.uid), {
+//             firstName,
+//             lastName,
+//             password,
+//             email: user.email,
+//           });
+//         }
+//         router.push("/dashboard");
+//       } else {
+//         setError("please verify your email before logging in");
+//       }
+//     } catch (error) {
+//       if (error instanceof Error) {
+//         setError(error.message);
+//       } else {
+//         setError("An unknown error occurred");
+//       }
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <form
+//          className="form"
+//         onSubmit={handleSigin}>
+//           <h1>Sign In</h1>
+
+//           <div className="input-group">
+//            <input
+//             className="input-group_input"
+//             placeholder=""
+//             type="email"
+//             // required
+//             value={email}
+//             onChange={(event) => setEmail(event.target.value)}
+//           />
+//             <label
+//              className="input-group_label"
+//             >
+//               Email </label>
+//              <span className="error">Not a valid Email</span>
+//           </div>
+
+        
+//           <div className="input-group">
+//             <input
+//             className="input-group_input"
+//             placeholder=""
+//             type="password"
+//             value={password}
+//             onChange={(event) => setPassword(event.target.value)}
+//           />
+//               <label
+//                className="input-group_label"
+//               >
+//                 Password</label>
+//           </div>
+         
+
+//           <button>Sign In</button>
+//           {/* {error && <p>{error}</p>} */}
+//           {error && <p style={{ color: "red" }}>{error}</p>}
+
+//           <p className="link">
+          
+//           <Link 
+//            className="forgotpass"
+//           href="/resetpassword">Forgot password</Link>
+//         </p>
+
+//           <p className="link">
+//             Don't have an account? <Link href="/signup">Sign up here</Link>
+//           </p>
+
+//           <div className="or">
+//               <div className="dash"></div>
+//               <div>OR</div>
+//               <div className="dash"></div>
+//              </div> 
+
+//           <button onClick={handleSiginwithGoogle}>Continue with google</button>
+//         </form>
+      
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Signin;
+
+
+
+
 "use client";
 import React, { useState } from "react";
-import  { useRouter } from "next/navigation";
-import { signInWithEmailAndPassword,  signInWithPopup } from "firebase/auth";
-import { auth, firestore } from "../firebase/firebase";
+import { useRouter } from "next/navigation";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth, firestore, googleAuthProvider } from "../firebase/firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import Link from "next/link";
-import { googleAuthProvider } from "../firebase/firebase";
 
- const Signin = () => {
+const SignIn = () => {
   const [email, setEmail] = useState("");
-  
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
-  const handleSiginwithGoogle = async () => {
+  const handleSignInWithGoogle = async () => {
     try {
       const userCredential = await signInWithPopup(auth, googleAuthProvider);
-      console.log(userCredential);
-
       const user = userCredential.user;
+
       if (user.emailVerified) {
-        // retrive user data from local storage
-
         const registrationData = localStorage.getItem("registrationData");
-        const {
-          firstName = "",
-          lastName = "",
-          password = "",
-        } = registrationData ? JSON.parse(registrationData) : {};
-
-        //check if user data is valid and exist in firebase
+        const { firstName = "", lastName = "", password = "" } = registrationData
+          ? JSON.parse(registrationData)
+          : {};
 
         const userDoc = await getDoc(doc(firestore, "user", user.uid));
         if (!userDoc.exists()) {
-
-          // save user data to firestre after email verification
-
           await setDoc(doc(firestore, "user", user.uid), {
             firstName,
             lastName,
@@ -46,18 +213,18 @@ import { googleAuthProvider } from "../firebase/firebase";
         }
         router.push("/dashboard");
       } else {
-        setError("please verify your email before logging in");
+        setError("Please verify your email before logging in.");
       }
     } catch (error) {
       if (error instanceof Error) {
         setError(error.message);
       } else {
-        setError("An unknown error occurred");
+        setError("An unknown error occurred.");
       }
     }
   };
 
-  const handleSigin = async (event: React.FormEvent) => {
+  const handleSignIn = async (event: React.FormEvent) => {
     event.preventDefault();
     setError(null);
 
@@ -70,16 +237,10 @@ import { googleAuthProvider } from "../firebase/firebase";
 
       const user = userCredential.user;
       if (user.emailVerified) {
-        // retrive user data from local storage
-
         const registrationData = localStorage.getItem("registrationData");
-        const {
-          firstName = "",
-          lastName = "",
-          password = "",
-        } = registrationData ? JSON.parse(registrationData) : {};
-
-        // check if user data is valid and exist in firebase
+        const { firstName = "", lastName = "", password = "" } = registrationData
+          ? JSON.parse(registrationData)
+          : {};
 
         const userDoc = await getDoc(doc(firestore, "user", user.uid));
         if (!userDoc.exists()) {
@@ -92,84 +253,70 @@ import { googleAuthProvider } from "../firebase/firebase";
         }
         router.push("/dashboard");
       } else {
-        setError("please verify your email before logging in");
+        setError("Please verify your email before logging in.");
       }
     } catch (error) {
       if (error instanceof Error) {
         setError(error.message);
       } else {
-        setError("An unknown error occurred");
+        setError("An unknown error occurred.");
       }
     }
   };
 
   return (
-    <>
-      <div>
-        <form
-         className="form"
-        onSubmit={handleSigin}>
-          <h1>Sign In</h1>
+    <div>
+      <form className="form" onSubmit={handleSignIn}>
+        <h1>Sign In</h1>
 
-          <div className="input-group">
-           <input
+        <div className="input-group">
+          <input
             className="input-group_input"
-            placeholder=""
+            placeholder="Email"
             type="email"
-            // required
             value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
-            <label
-             className="input-group_label"
-            >
-              Email </label>
-             <span className="error">Not a valid Email</span>
-          </div>
+          <label className="input-group_label">Email</label>
+          <span className="error">Not a valid Email</span>
+        </div>
 
-        
-          <div className="input-group">
-            <input
+        <div className="input-group">
+          <input
             className="input-group_input"
-            placeholder=""
+            placeholder="Password"
             type="password"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
-              <label
-               className="input-group_label"
-              >
-                Password</label>
-          </div>
-         
+          <label className="input-group_label">Password</label>
+        </div>
 
-          <button>Sign In</button>
-          {/* {error && <p>{error}</p>} */}
-          {error && <p style={{ color: "red" }}>{error}</p>}
+        <button type="submit">Sign In</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
 
-          <p className="link">
-          
-          <Link 
-           className="forgotpass"
-          href="/resetpassword">Forgot password</Link>
+        <p className="link">
+          <Link className="forgotpass" href="/resetpassword">
+            Forgot password
+          </Link>
         </p>
 
-          <p className="link">
-            Don't have an account? <Link href="/signup">Sign up here</Link>
-          </p>
+        <p className="link">
+          Don't have an account? <Link href="/signup">Sign up here</Link>
+        </p>
 
-          <div className="or">
-              <div className="dash"></div>
-              <div>OR</div>
-              <div className="dash"></div>
-             </div> 
+        <div className="or">
+          <div className="dash"></div>
+          <div>OR</div>
+          <div className="dash"></div>
+        </div>
 
-          <button onClick={handleSiginwithGoogle}>Continue with google</button>
-        </form>
-      
-      </div>
-    </>
+        <button type="button" onClick={handleSignInWithGoogle}>
+          Continue with Google
+        </button>
+      </form>
+    </div>
   );
-}
+};
 
-export default Signin;
+export default SignIn;
